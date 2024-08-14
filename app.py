8ab79af1e7b7d0728eb162fbbba0aad742ce012a
igord <igordiasth@gmail.com>
2024-08-14 11:42:32 -0400

    add `styles.css`, add `view_transactions.html`

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import pandas as pd
 import csv
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "sachin"  # Replace with your own secret key
@@ -31,8 +32,21 @@ class CSV:
         with open(cls.CSV_FILE, 'a', newline='') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=cls.COLUMNS)
             writer.writerow(new_entry)
-        flash('Transaction added successfully!', 'success')
-        print('Entry added successfully.')
+
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df['date'] = pd.to_datetime(df['date'], format=cls.FORMAT)
+
+        start_date = datetime.strptime(start_date, cls.FORMAT)
+        end_date = datetime.strptime(end_date, cls.FORMAT)
+
+        mask = (df['date'] >= start_date) & (df['date'] <= end_date)
+        filtered_df = df.loc[mask]
+
+        filtered_df['date'] = filtered_df['date'].dt.strftime(cls.FORMAT)
+
+        return filtered_df
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -44,18 +58,28 @@ def add_transaction():
         category = request.form['category']
         description = request.form['description']
         CSV.add_entry(date, amount, category, description)
+        flash('Transaction added successfully!', 'success')
         return redirect(url_for('index'))
     return render_template('add_transaction.html')
+
+
+@app.route('/transactions', methods=['GET', 'POST'])
+def view_transactions():
+    if request.method == 'POST':
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+
+        df = CSV.get_transactions(start_date, end_date)
+        transactions = df.to_dict(orient='records')
+
+        return render_template('view_transactions.html', transactions=transactions)
+
+    return render_template('view_transactions.html', transactions=[])
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/view')
-def view_transactions():
-    return render_template('view_transactions.html')
 
 
 if __name__ == '__main__':
