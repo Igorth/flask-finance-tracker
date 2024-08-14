f3a25b82136e1e82302fb67ece4742215f3d88b8
igord <igordiasth@gmail.com>
2024-08-14 10:07:30 -0400

    add the routes `app.py` and structure html `base.html`

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! This is a simple Flask application."
+    return render_template('index.html')
+
+
+@app.route('/add')
+def add_transaction():
+    return render_template('add_transaction.html')
+
+
+@app.route('/view')
+def view_transactions():
+    return render_template('view_transactions.html')
 
 
 if __name__ == '__main__':
